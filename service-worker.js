828bc4991ab570da8fd5b9574ae26311b106029d
Jonathan McDonnell <mcdonnell.jonathan@yahoo.com>
2022-07-18 21:06:54 -0400

    Build out caching functionality

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,53 @@
-// TODO: Add page chaching
+const APP_PREFIX = 'jvmcdonnell-';
+const VERSION = 'version01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+const FILES_TO_CACHE = [
+    "./index.html",
+    "./styles.css",
+    "./scripts.js"
+];
+
+self.addEventListener("install", function (e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            console.log('Installing cache: ' + CACHE_NAME);
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    )
+});
+
+self.addEventListener("activate", function (e) {
+    e.waitUntil(
+        caches.keys().then(function(keyList) {
+            let cacheKeeplist = keyList.filter(function(key) {
+                return key.indexOf(APP_PREFIX);
+            });
+            cacheKeeplist.push(CACHE_NAME);
+
+            return Promise.all(
+                keyList.map(function(key, i) {
+                    if (cacheKeeplist.indexOf(key) === -1) {
+                        console.log('deleting cache: ' + keyList[i]);
+                        return caches.delete(keyList[i]);
+                    }
+                })
+            )
+        })
+    );
+});
+
+self.addEventListener("fetch", function (e) {
+    console.log('fetch request: ' + e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function(req) {
+            if (req) {
+                console.log('responding with cache: ' + e.request.url);
+                return req;
+            } else {
+                console.log('file is not cached, fetching: ' + e.request.url);
+                return fetch(e.request);
+            }
+        })
+    )
+})
